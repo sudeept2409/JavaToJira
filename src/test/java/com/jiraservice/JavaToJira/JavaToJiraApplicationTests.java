de6809be24520b2d8b041b6d7e6a9307c0a7c58a
Sudeept Naval Mishra <131764951+sudeept2409@users.noreply.github.com>
2023-09-03 23:37:43 +0530

    uyy

---
+++ src/test/java/com/jiraservice/JavaToJira/JavaToJiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiraservice.JavaToJira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaToJiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
